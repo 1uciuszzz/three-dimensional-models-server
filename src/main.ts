39a2e339d98aae1f01331efc92d7399f15bb73f6
1uciuszzz <nick_zzhao@outlook.com>
2023-03-01 15:32:20 +0800

    .

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { PrismaService } from "./prisma.service";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { logger: ["log"] });
 
   const docCfg = new DocumentBuilder()
     .setTitle("Three Dimensional Models Server API Reference")
